ae76d393b6b3ff769262628c2a51af7bfbc4fbf8
Ajay Anand <ajay.arthur@gmail.com>
2021-07-14 23:24:06 +0400

    Implemented updating schedule

--- post.py
+++ post.py
@@ -7,11 +7,18 @@ dotenv.load_dotenv()
 
 print("Logging in...")
 bot = Client()
-bot.login(os.getenv("USER"),os.getenv("PASS"))
+#bot.login(os.getenv("USER"),os.getenv("PASS"))
+
+def update_schedule(posts):
+    os.remove('schedule.txt')
+    with open('schedule.txt', 'w') as f:
+        for post in posts:
+            f.write(str(post))
+            f.write('\n')
 
 posts = []
 print("Bot Running")
-while len(posts) == 0:
+while True:
     with open('schedule.txt', 'r') as f:
         for line in f:
             if line != '':
@@ -21,6 +28,30 @@ while len(posts) == 0:
                 posts.append(line)
     
     if len(posts) > 0:
-        for post in posts:
-            if datetime.now() >= datetime.strptime(post[0], '%m/%d/%y %H:%M:%S'):
-                print(post)
+        i=0
+        length = len(posts)
+        while i < length:
+            scheduled_date = datetime.strptime(posts[i][0], '%m/%d/%y %H:%M:%S')
+            if datetime.now() >= scheduled_date:
+                if posts[i][1] == 'single_photo':
+                    #bot.photo_upload(path=posts[i][2], caption=posts[i][3])
+                    print(f"Uploaded post scheduled for {scheduled_date}")
+                    posts.pop(i)
+                    update_schedule(posts)
+                    i=0
+                elif posts[i][1] == 'single_video':
+                    #bot.video_upload(path=posts[i][2], caption=posts[i][3])
+                    print(f"Uploaded post scheduled for {scheduled_date}")
+                    posts.pop(i)
+                    update_schedule(posts)
+                    i=0
+                elif posts[i][1] == 'album':
+                    #bot.album_upload(path=posts[i][2], caption=posts[i][3])
+                    print(f"Uploaded post scheduled for {scheduled_date}")
+                    posts.pop(i)
+                    update_schedule(posts)
+                    i=0
+            else:
+                i+=1
+            
+            length = len(posts)

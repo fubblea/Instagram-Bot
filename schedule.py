c163ed35c39eeb93107199398a2199d60a8710f6
Ajay Anand <ajay.arthur@gmail.com>
2021-07-14 22:17:16 +0400

    Mutiple schedule types

--- schedule.py
+++ schedule.py
@@ -37,13 +37,59 @@ def photo_post():
     os.remove('temp.txt')
     
     with open('schedule.txt', 'a') as f:
-        f.write('\n' + str([date, 'single_photo', path, caption]))
+        f.write(str([date, 'single_photo', path, caption]) + '\n')
+    return
+
+def video_post():
+    print("Single video upload")
+    print("Select media")
+    
+    path = find_single_media()
+    if path == '':
+        return
+    
+    caption = input("Enter video caption, enter '~' to return to main menu: ")
+    if caption == '~':
+        return
+    
+    time_picker.pick()
+    
+    with open('temp.txt', 'r') as f:
+        date = f.read()
+    os.remove('temp.txt')
+    
+    with open('schedule.txt', 'a') as f:
+        f.write(str([date, 'single_video', path, caption]) + '\n')
+    return
+
+def album_post():
+    print("Album upload")
+    print("Select media")
+    
+    path = find_multiple_media()
+    if path == '':
+        return
+    
+    caption = input("Enter album caption, enter '~' to return to main menu: ")
+    if caption == '~':
+        return
+    
+    time_picker.pick()
+    
+    with open('temp.txt', 'r') as f:
+        date = f.read()
+    os.remove('temp.txt')
+    
+    with open('schedule.txt', 'a') as f:
+        f.write(str([date, 'album', path, caption]) + '\n')
     return
 
 while True:
     print("-----Main Menu-----")
     print("0. Exit")
     print("1. Schedule Single-Image Post")
+    print("2. Schedule Single-Video Post")
+    print("2. Schedule Album Post")
     
     try:
         option = int(input("Enter option: "))
@@ -54,5 +100,9 @@ while True:
         sys.exit()
     elif option == 1:
         photo_post()
+    elif option == 2:
+        video_post()
+    elif option == 3:
+        album_post()
     else:
         print("Invalid Option")
